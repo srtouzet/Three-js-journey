3e50dcf9097397357e5b898a1f672b1970d2d428
ludocativa <Srtouzet@hotmail.com>
2024-01-14 20:28:18 -0300

    04-transform-objects

---
+++ 04-transform-objects/src/script.js
@@ -0,0 +1,93 @@
+import * as THREE from 'three'
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Objects
+ */
+// const geometry = new THREE.BoxGeometry(1, 1, 1)
+// const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+// const mesh = new THREE.Mesh(geometry, material)
+const group = new THREE.Group()
+group.position.y = 1
+group.scale.y = 2
+group.rotation.y = 1
+scene.add(group)
+
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color: 0xff0000})
+)
+group.add(cube1)
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color: 0x00f000})
+)
+cube2.position.x = -2
+group.add(cube2)
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color: 0x000fff})
+)
+cube3.position.x = 2
+group.add(cube3)
+// Mesh positions
+// mesh.position.x = 0.7
+// mesh.position.y = - 0.6
+// mesh.position.z = -1
+
+// Cambio las 3 posiciones a la vez
+// mesh.position.set(0.7,-0.6,1)
+// scene.add(mesh)
+
+// Scale
+// mesh.scale.x = 2
+// mesh.scale.y = 0.5
+// mesh.scale.z = 0.5
+// mesh.scale.set(2,0.5,0.5)
+
+// Rotation
+// Reorder rotation
+// mesh.rotation.reorder('YXZ')
+// Media rotacion - half rotation
+// mesh.rotation.y = Math.PI
+// Full rotacion - full revolution
+// mesh.rotation.y = Math.PI * 0.5
+
+// console.log(Math.PI);
+
+// Axes helper
+const axesHelper = new THREE.AxesHelper(2)
+scene.add(axesHelper)
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+/**
+ * Camera
+ */
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+scene.add(camera)
+// camera.lookAt(mesh.position)
+
+// Distancia entre la mesh y la camera
+// console.log(mesh.position.distanceTo(camera.position))
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
